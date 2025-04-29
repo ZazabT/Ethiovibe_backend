c56afeeb95a54c90b47a7a821e96ceca27763ad7
Tizazab <tizazabayana2@gmail.com>
2025-04-29 02:02:37 -0700

    Started product model

---
+++ models/Product.model.js
@@ -0,0 +1,113 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true, 
+        index: true,
+    },
+    description: {
+        type: String,
+        required: true, 
+    },
+    price: {
+        type: Number,
+        required: true, 
+        min: 0,
+    },
+    discountPercentage: {
+        type: Number,
+        required: true, 
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    stockStatus: {
+        type: String,
+        enum: ['IN_STOCK', 'OUT_OF_STOCK', 'LOW_STOCK'],
+        default: 'IN_STOCK',
+      },
+    sku: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    sizes: {
+        type: [String],
+        required: true, 
+    },
+    colors: {
+        type: [String],
+        required: true, 
+    },
+    images: [
+        {
+          url: { type: String, required: true },
+          isPrimary: { type: Boolean, default: false },
+          altText: { type: String },
+        },
+      ],
+    gender: {
+        type: String,
+        required: true, 
+        enum : ['male', 'female', 'unisex'],
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+   
+    },
+    isPublished: {
+        type: Boolean,
+        default: false, 
+    },
+    ratings: {
+        type: Number,
+        default: 0,
+    },
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
+    tags: {
+        type: [String],
+    },
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true, 
+    },
+    metaTitle: {
+        type: String,
+    },
+    metaDescription: {
+        type: String,
+    },
+    metaKeywords: {
+        type: String,
+    },
+    dimensions: {
+        length: { type: Number, min: 0 },
+        width: { type: Number, min: 0 },
+        height: { type: Number, min: 0 },
+    },
+    weight: {
+       type: Number, 
+       min: 0,
+    }
+
+}
+, {
+    timestamps: true,
+}
+);
+module.exports = mongoose.model('Product' , productSchema);
