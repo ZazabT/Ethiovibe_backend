f73e7a15c76a1a49bf180c3b9736b9da242acebe
Tizazab <tizazabayana2@gmail.com>
2025-05-05 04:43:30 -0700

    Added subscriber model

--- controllers/Order.controller.js
+++ controllers/Order.controller.js
@@ -22,3 +22,30 @@ exports.getMyOrder = async (req, res) => {
         });
     }
 }
+
+
+exports.getOrderDetails = async (req, res) => {
+   const {id} = req.params; 
+
+   try {
+
+    // find order by id
+    const order = await Order.findById(id).populate('user', 'name email');
+
+    if(!order){
+        return res.status(404).json({message : 'Order not found'});
+    }
+    
+    res.status(200).json({msg : 'Order Fetched Successfully', order});
+   } catch (error) {
+    console.error('💥Fetching error:', {
+        message: error.message,
+        stack: error.stack,
+        context: { id },
+    });
+    return res.status(500).json({
+        error: 'Server Error',
+        msg: error.message || 'An unexpected error occurred', 
+    });
+   }
+}
